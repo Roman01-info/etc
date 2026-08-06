fbcb21feb75bd674d9b4f38b2564a8b5c3f667f3
roman <modhubony22@gmail.com>
2026-08-06 23:40:51 +0600

    update icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
     title: "ETC - Elevated Travel Corporation | Luxury Travel Agency  | etc travel bd",
   description: "Comprehensive travel solutions tailored to your every need. Expert visa processing, luxury hotels, and premium flight bookings worldwide.",
   icons: {
-    icon: "/gallery/etc_main_logo.png",
+    icon: "/gallery/etc_main_icon.png",
     shortcut: "/gallery/etc_main_logo.png",
     apple: "/gallery/etc_main_logo.png",
   },
